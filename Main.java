1955a442e3ab04008d614d3c3e57ceaa204ea15e
madhu33 <madhu.koseke@gmail.com>
2016-09-19 00:22:18 +0530

    A new line from Remote

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main
 	{
 		System.out.println("Hello World!");
 		System.out.println("Modified line from local repository");
-
+		System.out.println("This is new line from remote repository");
 	}
 }

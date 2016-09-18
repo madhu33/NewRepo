32c166b93a1f3dddb48a3cf7e18ab712f95577e7
madhu33 <madhu.koseke@gmail.com>
2016-09-19 01:06:49 +0530

    Edit from Eclipse

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ class Main
 		System.out.println("Modified line from local repository");
 		System.out.println("This is new line from remote repository");
 		System.out.println("New line from Developer Branch");
+		System.out.println("A new line from Eclipse");
 	}
 }
